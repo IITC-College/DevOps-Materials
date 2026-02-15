f8f784151c20768e434a2a5f7f8a9adf547dd914
Lirone Fitoussi <lironefit@icloud.com>
2026-02-15 14:17:29 +0200

    Add Express server setup to index.js for Docker optimization lab. This change introduces a basic Express application that responds with a message, enhancing the functionality of the lab example.

--- # Instructor Content/Docker/8/Optimizing Images/index.js
+++ # Instructor Content/Docker/8/Optimizing Images/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello from express");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
