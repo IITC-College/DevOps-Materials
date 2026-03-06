cb820f6d331c32708d2de45d6b432e5835e2ec80
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2026-03-06 15:15:10 +0200

    done project

--- Instructor Content/K8s/10 - Project - Deploying a MongoDB Database/STEP 9/color-api/src/index.js
+++ Instructor Content/K8s/10 - Project - Deploying a MongoDB Database/STEP 9/color-api/src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser")
-
+const mongoose = require("mongoose");
 // Routers
 const healthRouter = require("./routes/health");
 const apiRouter = require("./routes/api");
